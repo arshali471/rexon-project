51aa5fa21bedabcbdc5bb7ff0cf7758ad72f4622
arshali471 <arshali471@gmail.com>
2021-07-25 14:02:44 +0530

    add link

--- src/Components/Navb.jsx
+++ src/Components/Navb.jsx
@@ -10,17 +10,17 @@ function Navb() {
         <div>
             <Navbar bg="light" expand="lg" className='p-4'>
                 <Container>
-                    <Navbar.Brand href="#home">Geocoal Engineering pvt. Ltd</Navbar.Brand>
+                    <Navbar.Brand href="/">Geocoal Engineering pvt. Ltd</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="#home" className='p-3'>Home</Nav.Link>
-                            <Nav.Link href="#link" className='p-3'>About</Nav.Link>
-                            <Nav.Link href="#ourgallery" className='p-3'>Our gallery</Nav.Link>
-                            <Nav.Link href="#services" className='p-3'>Services</Nav.Link>
+                            <Nav.Link href="/" className='p-3'>Home</Nav.Link>
+                            <Nav.Link href="/about" className='p-3'>About</Nav.Link>
+                            <Nav.Link href="/gallery" className='p-3'>Our gallery</Nav.Link>
+                            <Nav.Link href="/services" className='p-3'>Services</Nav.Link>
                            
-                            <Nav.Link className='p-3' exact activeClassName="active_class" to='/'>Career</Nav.Link>
-                            <Nav.Link className='p-3' exact activeClassName="active_class" to='/contact'>Contact Us</Nav.Link>
+                            <Nav.Link className='p-3' exact activeClassName="active_class" href='/career'>Career</Nav.Link>
+                            <Nav.Link className='p-3' exact activeClassName="active_class" href='/contact'>Contact Us</Nav.Link>
                         
                         </Nav>
                     </Navbar.Collapse>
--- src/Components/ReactRouter.jsx
+++ src/Components/ReactRouter.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import Career from './Career';
+import Services from './Services';
+import Gallery from './Gallery';
+import Home from './Home';
+import About from './About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navb from './Navb';
 import Footer from './Footer';
 import Contact from './Contact'; 
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom'
 
 function ReactRouter() {
     return (
         <div>
             <Navb />
-            <BrowserRouter>
+            <BrowserRouter>   
             <Switch>
-                <Route exact path="/" component={ Career} />
+                <Route exact path="/career" component={ Career} />
+                <Route exact path="/" component={ Home} />
+                <Route exact path="/about" component={ About} />
+                <Route exact path="/gallery" component={ Gallery} />
+                <Route exact path="/services" component={ Services} />
                 <Route path="/contact" component={ Contact} />
                 <Route component={Error} />
 
